89653007678ebc73edd6616b8604366e4112b838
KaroloLykos <l.g.karolos@gmail.com>
2021-12-08 01:26:14 +0200

    add:batach render quads

--- OpenGL-BatchRendering/src/BatchRenderingLayer.cpp
+++ OpenGL-BatchRendering/src/BatchRenderingLayer.cpp
@@ -4,6 +4,7 @@ using namespace GLCore;
 using namespace GLCore::Utils;
 
 BatchRenderingLayer::BatchRenderingLayer()
+	:m_CameraController(16.0f / 9.0f)
 {
 }
 
@@ -15,7 +16,43 @@ void BatchRenderingLayer::OnAttach()
 {
 	EnableGLDebugging();
 
-	// Init here
+	m_Shader = Shader::FromGLSLTextFiles(
+		"assets/shaders/shader.glsl.vert",
+		"assets/shaders/shader.glsl.frag"
+	);
+
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+	float vertices[] = {
+		-1.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f,
+		-1.5f,  0.5f, 0.0f,
+
+		 0.5f, -0.5f, 0.0f,
+		 1.5f, -0.5f, 0.0f,
+		 1.5f,  0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f
+	};
+
+	glCreateVertexArrays(1, &m_QuadVA);
+	glBindVertexArray(m_QuadVA);
+
+	glCreateBuffers(1, &m_QuadVB);
+	glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexArrayAttrib(m_QuadVB, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+	uint32_t indices[] = {
+		0,1,2,2,3,0,
+		4,5,6,6,7,4
+	};
+
+	glCreateBuffers(1, &m_QuadIB);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void BatchRenderingLayer::OnDetach()
@@ -25,12 +62,37 @@ void BatchRenderingLayer::OnDetach()
 
 void BatchRenderingLayer::OnEvent(Event& event)
 {
-	// Events here
+	m_CameraController.OnEvent(event);
+}
+
+static void SetUniformMat4(uint32_t shader, const char* name, const glm::mat4& matrix)
+{
+	int loc = glGetUniformLocation(shader, name);
+	glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(matrix));
+}
+
+static void SetUniform4fv(uint32_t shader, const char* name, const glm::vec4& matrix)
+{
+	int loc = glGetUniformLocation(shader, name);
+	glUniform4fv(loc, 1, glm::value_ptr(matrix));
 }
 
 void BatchRenderingLayer::OnUpdate(Timestep ts)
 {
-	// Render here
+	m_CameraController.OnUpdate(ts);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(m_Shader->GetRendererID());
+
+	SetUniform4fv(m_Shader->GetRendererID(), "u_Color", m_SquareColor);
+
+	SetUniformMat4(m_Shader->GetRendererID(), "u_ViewProj", m_CameraController.GetCamera().GetViewProjectionMatrix());
+
+	glBindVertexArray(m_QuadVA);
+
+	SetUniformMat4(m_Shader->GetRendererID(), "u_Transform", glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
 }
 
 void BatchRenderingLayer::OnImGuiRender()
